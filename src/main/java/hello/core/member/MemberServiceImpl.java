71b5d9da03aba46e34c31a16f744e39474de4e26
이강현 <khlee@nshc.net>
2022-06-09 22:51:21 +0900

    0609

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,16 +1,19 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
-
-
 
     @Override
     public void join(Member member) {
@@ -22,7 +25,6 @@ public class MemberServiceImpl implements MemberService{
         return memberRepository.findById(memberId);
     }
 
-    
     //테스트 용도
     public MemberRepository getMemberRepository() {
         return memberRepository;
