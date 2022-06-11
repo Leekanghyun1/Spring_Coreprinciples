448135bdcc7cab818c447c3b2daa392864a2c568
이강현 <khlee@nshc.net>
2022-06-12 00:03:07 +0900

    0611

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,10 +1,12 @@
 package hello.core;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
+
 
 	@Test
 	void contextLoads() {
